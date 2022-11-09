0be06efdc25a9683321d0c895b623e24ca7b2e61
ahnafi <nafxd9896@gmail.com>
2022-11-09 11:05:45 +0700

    meringkas javascript

--- script.js
+++ script.js
@@ -12,48 +12,65 @@ function gethasil(comp, player) {
   if (player == "semut") return comp == "gajah" ? "MENANG!" : "KALAH!";
 }
 
-const pgajah = document.querySelector(".gajah");
-pgajah.addEventListener("click", function () {
-  const pilihcomp = getcomp();
-  const pilihplayer = pgajah.className;
-  const hasil = gethasil(pilihcomp, pilihplayer);
-  // console.log('computer '+ pilihcomp)
-  // console.log('player ' + pilihplayer)
-  // console.log(gethasil(pilihcomp,pilihplayer))
-
-  const imgcomp = document.querySelector(".computer img");
-  imgcomp.setAttribute("src", "img/" + pilihcomp + ".png");
-
-  const fight = document.getElementById("fight");
-  fight.innerHTML = hasil;
+//mempersingkat codingan
+const pilihan = document.querySelectorAll("li img");
+pilihan.forEach(function (i) {
+  i.addEventListener("click", function () {
+    const pilihcomp = getcomp();
+    const pilihplayer = i.className;
+    const hasil = gethasil(pilihcomp, pilihplayer);
+    const imgcomp = document.querySelector(".computer img");
+    imgcomp.setAttribute("src", "img/" + pilihcomp + ".png");
+    const fight = document.getElementById("fight");
+    fight.innerHTML = hasil;
+  });
 });
-const porang = document.querySelector(".orang");
-porang.addEventListener("click", function () {
-  const pilihcomp = getcomp();
-  const pilihplayer = porang.className;
-  const hasil = gethasil(pilihcomp, pilihplayer);
-  // console.log('computer '+ pilihcomp)
-  // console.log('player ' + pilihplayer)
-  // console.log(gethasil(pilihcomp,pilihplayer))
 
-  const imgcomp = document.querySelector(".computer img");
-  imgcomp.setAttribute("src", "img/" + pilihcomp + ".png");
+//codingan lama
 
-  const fight = document.getElementById("fight");
-  fight.innerHTML = hasil;
-});
-const psemut = document.querySelector(".semut");
-psemut.addEventListener("click", function () {
-  const pilihcomp = getcomp();
-  const pilihplayer = psemut.className;
-  const hasil = gethasil(pilihcomp, pilihplayer);
-  // console.log('computer '+ pilihcomp)
-  // console.log('player ' + pilihplayer)
-  // console.log(gethasil(pilihcomp,pilihplayer))
 
-  const imgcomp = document.querySelector(".computer img");
-  imgcomp.setAttribute("src", "img/" + pilihcomp + ".png");
+// const pgajah = document.querySelector(".gajah");
+// pgajah.addEventListener("click", function () {
+//   const pilihcomp = getcomp();
+//   const pilihplayer = pgajah.className;
+//   const hasil = gethasil(pilihcomp, pilihplayer);
+//   // console.log('computer '+ pilihcomp)
+//   // console.log('player ' + pilihplayer)
+//   // console.log(gethasil(pilihcomp,pilihplayer))
 
-  const fight = document.getElementById("fight");
-  fight.innerHTML = hasil;
-});
+//   const imgcomp = document.querySelector(".computer img");
+//   imgcomp.setAttribute("src", "img/" + pilihcomp + ".png");
+
+//   const fight = document.getElementById("fight");
+//   fight.innerHTML = hasil;
+// });
+// const porang = document.querySelector(".orang");
+// porang.addEventListener("click", function () {
+//   const pilihcomp = getcomp();
+//   const pilihplayer = porang.className;
+//   const hasil = gethasil(pilihcomp, pilihplayer);
+//   // console.log('computer '+ pilihcomp)
+//   // console.log('player ' + pilihplayer)
+//   // console.log(gethasil(pilihcomp,pilihplayer))
+
+//   const imgcomp = document.querySelector(".computer img");
+//   imgcomp.setAttribute("src", "img/" + pilihcomp + ".png");
+
+//   const fight = document.getElementById("fight");
+//   fight.innerHTML = hasil;
+// });
+// const psemut = document.querySelector(".semut");
+// psemut.addEventListener("click", function () {
+//   const pilihcomp = getcomp();
+//   const pilihplayer = psemut.className;
+//   const hasil = gethasil(pilihcomp, pilihplayer);
+//   // console.log('computer '+ pilihcomp)
+//   // console.log('player ' + pilihplayer)
+//   // console.log(gethasil(pilihcomp,pilihplayer))
+
+//   const imgcomp = document.querySelector(".computer img");
+//   imgcomp.setAttribute("src", "img/" + pilihcomp + ".png");
+
+//   const fight = document.getElementById("fight");
+//   fight.innerHTML = hasil;
+// });
